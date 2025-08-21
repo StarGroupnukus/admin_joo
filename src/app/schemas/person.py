ed28f883f4d03be192e72d20d9d714280e60e007
Babur Parakhatov <melomanovmeloman@gmail.com>
2025-08-21 11:13:17 +0500

    image url delete

--- src/app/schemas/person.py
+++ src/app/schemas/person.py
@@ -28,8 +28,7 @@ class PersonFilter(PersonBase):
     image_url : str | None = None
     department_id : int | None = None
 
-class PersonUpdate(PersonBase):
-    image_url: str | None = None
+class PersonUpdate(BaseModel):
     first_name: str | None = None
     last_name: str | None = None
     department_id: int | None = None
